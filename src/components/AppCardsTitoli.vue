42919f792cbf82b64cdd00c7583ac9de35d4403a
Rocca-Francesco <roccafrance98@yahoo.it>
2023-03-06 15:04:13 +0100

    loop flag

--- src/components/AppCardsTitoli.vue
+++ src/components/AppCardsTitoli.vue
@@ -1,8 +1,26 @@
 <script>
 export default {
+  data() {
+    return {
+      // dichiaro le variabili che userò nella generazione delle bandiere
+      flag: '',
+      flags: [],
+      urlFlag: "https://flagsapi.com/",
+    }
+  },
+
   props: {
     listMovies: Array,
     listTVseries: Array,
+  },
+
+  methods: {
+    generateFlags(nation) {
+      nation = nation.toUpperCase();
+      console.log(nation);
+      this.flag = this.urlFlag + nation + '/flat/32.png';
+      return this.flag;
+    }
   }
 }
 </script>
@@ -20,12 +38,18 @@ export default {
   <div class="col-3 text-center" v-for="TVserie in listTVseries">
     <h3>{{ TVserie.original_name }}</h3>
     <h2>{{ TVserie.name }}</h2>
-    <span class="d-block">{{ TVserie.original_language }}</span>
+    <span class="d-block">
+      <img :src="generateFlags(TVserie.original_language)" width="30">
+    </span>
     <span>{{ TVserie.vote_average }}</span>
   </div>
 </template>
 
 <style lang="scss" scoped>
+h2 {
+  font-size: 1.5rem;
+}
+
 h3 {
   font-size: 0.5rem;
 }
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -21,29 +21,33 @@ export default {
   <main>
     <!-- aggiungo un v-show per non visualizzare i titoli all'inizio ma anche nell'eventualità non ci siano risultati -->
     <div id="film" v-show="storeMovies.movies.length > 0">
-    <h2>FILM</h2>
     <div class="container">
+      <h2>FILM</h2>
         <div class="row">
           <AppCardsTitoli :listMovies="storeMovies.movies" />
         </div>
       </div>
       <!-- <ul>
-            <li v-for="movie in storeMovies.movies">{{ movie.title }}</li>
-          </ul> -->
+                <li v-for="movie in storeMovies.movies">{{ movie.title }}</li>
+              </ul> -->
     </div>
     <!-- aggiungo un v-show per non visualizzare i titoli all'inizio ma anche nell'eventualità non ci siano risultati -->
     <div id="tvSeries" v-show="storeTVSeries.tvSeries.length > 0">
-      <h2>SERIE TV</h2>
       <div class="container">
+        <h2>SERIE TV</h2>
         <div class="row">
           <AppCardsTitoli :listTVseries="storeTVSeries.tvSeries" />
         </div>
       </div>
       <!-- <ul>
-          <li v-for="series in storeTVSeries.tvSeries">{{ series.name }}</li>
-        </ul> -->
+              <li v-for="series in storeTVSeries.tvSeries">{{ series.name }}</li>
+            </ul> -->
     </div>
   </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h2 {
+  font-size: 1rem;
+}
+</style>
